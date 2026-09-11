1b939010aacf42fe8dad04b2f0f1f3c27734470c
Drew-Kirby <159677238+Drew-Kirby@users.noreply.github.com>
2026-09-11 14:40:03 -0400

    Load image's pixel data into a 2D array.

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 from PIL import Image
 
-img = Image.open("ascii-pineapple.jpg")
+image = ("ascii-pineapple.jpg")
 
-if img._open:
-    print("Successfully loaded image!")
-    print("Image size: ", img.width, "x", img.height)
-else:
-    print("error")
+def readImg(im):
+    img = Image.open(im)
+
+    if img._open:
+        print("Successfully loaded image!")
+        print("Image size: ", img.width, "x", img.height)
+    else:
+        print("error")
+
+def loadImgData(im):
+    img = Image.open(im)
+
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+
+    pixels = []
+
+    for y in range(img.height):
+        row = []
+
+        for x in range(img.width):
+            pixel = img.getpixel((x,y))
+            row.append(pixel)
+
+        pixels.append(row)
+
+    print("Rows: ", len(pixels))
+    print("Columns: ", len(pixels[0]))
+
+loadImgData(image)
+readImg(image)
